d0b73935f3654b32a7c9beb4f8082ab4adae1b20
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-02-14 18:55:11 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,1 +1,2 @@
+Get-Date; Start-Sleep -Seconds 5; Get-Date; Start-Sleep -Seconds 5;
 Start-Process nohup 'cmd.exe /k echo X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H* >> eicar.com'
