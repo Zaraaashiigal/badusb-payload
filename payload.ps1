93679696b703d129596018c1a5f0cf818cc2f70d
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-02-15 12:22:59 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,3 @@
 $url = "raw.githubusercontent.com/Zaraaashiigal/badusb-payload/main/sowl.wav"
 Invoke-WebRequest -Uri $url -OutFile "$env:LOCALAPPDATA/sowl.wav"
-(New-Object Media.SoundPlayer "$env:LOCALAPPDATA/sowl.wav").play();
+(New-Object Media.SoundPlayer "$env:LOCALAPPDATA/sowl.wav").playSync();
