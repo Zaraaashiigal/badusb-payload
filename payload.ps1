248be6b875c0a8ba08aec50e30b9dd0ac6de0220
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-02-15 11:32:36 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,1 @@
-$url = "https://live.sysinternals.com/procexp64.exe"
-Invoke-WebRequest -Uri $url -OutFile "$env:LOCALAPPDATA/myexe.exe"
-Start-Process -Filepath "$env:LOCALAPPDATA/myexe.exe"
+start "https://www.youtube.com/watch?v=xvFZjo5PgG0"
